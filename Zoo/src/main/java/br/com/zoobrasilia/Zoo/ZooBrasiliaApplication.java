ab61b9a3d2804de5d543e253050a6d38264559d5
pedro.pasv <paventura@stefanini.com>
2020-12-08 17:54:34 -0300

    Initial Commit

---
+++ Zoo/src/main/java/br/com/zoobrasilia/Zoo/ZooBrasiliaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zoobrasilia.Zoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooBrasiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooBrasiliaApplication.class, args);
+	}
+
+}
